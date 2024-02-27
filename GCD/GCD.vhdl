0cada5eb4f520f8d691cd41b4d4ce7c73ac60389
Niranjan Dahal <85100536+niranjandahal@users.noreply.github.com>
2024-02-27 23:21:14 +0545

    Update GCD.vhdl

--- GCD/GCD.vhdl
+++ GCD/GCD.vhdl
@@ -1,60 +1,78 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.all;
 
-entity GCD is
-  port
-  (
-    RESET      : in std_logic;
-    CLK        : in std_logic;
-    GO         : in std_logic;
-    NUM1       : in integer;
-    NUM2       : in integer;
-    Result_GCD : out integer); -- Renamed the output signal to avoid name conflict
-end GCD;
-architecture Behavioral of GCD is
-  type state is (START, INPUT, CALCULATE, OUTPUT);
-  signal PS, NS : state;
-  signal X, Y   : integer;
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
 
-begin
-  seq_proc : process (CLK, RESET)
-  begin
-    if (RESET = '1') then
-      PS <= START;
-    elsif (rising_edge(CLK)) then
-      PS <= NS;
-    end if;
-  end process seq_proc;
 
-  comb_proc : process (NUM1, NUM2, PS)
-    variable A, B : integer;
-  begin
-    case PS is
-      when START =>
-        Result_GCD <= 0; -- Changed from GCD to Result_GCD
-        NS         <= INPUT;
-      when INPUT =>
-        X  <= NUM1;
-        Y  <= NUM2;
-        NS <= CALCULATE;
-      when CALCULATE =>
-        A := X;
-        B := Y;
-        while A /= B loop
-          if A > B then
-            A := A - B;
-          else
-            B := B - A;
-          end if;
-        end loop;
-        Result_GCD <= A; -- Changed from GCD to Result_GCD
-        NS         <= OUTPUT;
-      when OUTPUT =>
-        NS <= START;
-      when others =>
-        Result_GCD <= 0; -- Changed from GCD to Result_GCD
-        NS         <= START;
-    end case;
 
-  end process comb_proc;
-end Behavioral;
+ENTITY GCD IS
+    PORT (
+        RESET : IN STD_LOGIC;
+        CLK : IN STD_LOGIC;
+        GO : IN STD_LOGIC;
+        NUM1 : IN INTEGER;
+        NUM2 : IN INTEGER;
+        GCD : OUT INTEGER);
+END GCD;
+
+ARCHITECTURE Behavioral OF GCD IS
+
+    TYPE state IS (START, INPUT, CHECK, UPDATE_X, UPDATE_Y, OUTPUT);
+
+    SIGNAL PS, NS : state;
+
+BEGIN
+
+    seq_proc : PROCESS (CLK, GO, RESET)
+    BEGIN
+        IF (GO = '1') THEN
+            IF (RESET = '1') THEN
+                PS <= START;
+            ELSIF (rising_edge(CLK)) THEN
+                PS <= NS;
+            END IF;
+        END IF;
+    END PROCESS seq_proc;
+
+    comb_proc : PROCESS (NUM1, NUM2, PS) VARIABLE X, Y : INTEGER;
+
+    BEGIN
+
+        CASE PS IS
+
+            WHEN START =>
+                GCD <= 0;
+                NS <= INPUT;
+
+            WHEN INPUT =>
+                X := NUM1;
+                Y := NUM2;
+                NS <= CHECK;
+
+            WHEN CHECK =>
+                IF (X > Y) THEN
+                    NS <= UPDATE_X;
+                ELSIF (X < Y) THEN
+                    NS <= UPDATE_Y;
+                ELSE
+                    NS <= OUTPUT;
+                END IF;
+
+            WHEN UPDATE_X =>
+                X := X - Y;
+                NS <= CHECK;
+
+            WHEN UPDATE_Y =>
+                Y := Y - X;
+                NS <= CHECK;
+
+            WHEN OUTPUT =>
+                GCD <= X;
+                NS <= INPUT;
+
+            WHEN OTHERS =>
+                GCD <= 0;
+                NS <= INPUT;
+
+        END CASE;
+    END PROCESS comb_proc;
+END Behavioral;
